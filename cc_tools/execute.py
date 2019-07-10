5280134276b565b697f159915f522f95c02a9a92
Ryan Bradley <bradleyrp@gmail.com>
2019-07-10 14:53:05 -0400

    temporary fix for squashfs tools during pull

--- cc_tools/execute.py
+++ cc_tools/execute.py
@@ -172,7 +172,7 @@ local images_dn = "%(image_spot)s"
 local target = "%(target)s"
 local source = "%(source)s"
 
-load('singularity')
+load('cc/singularity')
 
 function resolve_tilde(s)
     return(s:gsub("^~",os.getenv("HOME")))
@@ -187,14 +187,17 @@ if mode()=="load" then
         lfs.mkdir(images_dn_abs)
     end
     if lfs.attributes(target_fn,'mode')==nil then
-        -- cc/env includes squashfs tools
-        -- !! conda.lua and env.lua were conflicting so I hardcoded one in the other
-        load('cc/env')
-        io.stderr:write("[CC] fetching " .. target .. "\\n")
+        -- if squashfs comes from cc/env we prepend the path here
+        -- this is a hack in place of a ml cc/env which would be much more elegant but could not be unloaded
+        -- utterly failed to load cc/env and unload it. something appears to be asynchronous (beyond just the lmod execute)
+        append_path("PATH",pathJoin(os.getenv("_COMCOL_ROOT"),"miniconda/envs/community-collections/bin"))
         local cmd = 'singularity pull ' .. target_fn .. ' ' .. source
-        execute {cmd=cmd,modeA={"load"}}
-        -- !! should we conditionally unload this if it was not loaded before, for opaqueness?
-        unload('cc/env')
+        -- execute {cmd=cmd,modeA={"load"}}
+		os.execute(cmd)
+        -- cleanup the squashfs hack (note: it would be nice to only remove this 
+        --   if it was not in there before to avoid conflicts with cc/env)
+        remove_path("PATH",pathJoin(os.getenv("_COMCOL_ROOT"),"miniconda/envs/community-collections/bin"))
+        io.stderr:write("[CC] downloaded the image: " .. target_fn .. "\\n")
     end
     set_shell_function('%(bin_name)s',"singularity run " .. target_fn,"singularity run " .. target_fn)
 end
@@ -220,7 +223,7 @@ class ModuleFileBase(Handler):
         detail['target'] = 'julia.sif'
         #! detail['source'] = 'library://sylabs/examples/julia:latest'
         detail['source'] = 'docker://julia'
-        detail['bin_name'] = 'julia'
+        detail['bin_name'] = 'latest'
         """
         modulefiles:
             1. base modulefile for latest links to a version number
