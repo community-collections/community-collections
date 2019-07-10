2afafe02fb025401cd6eaad30c08f2c0cb55822a
Ryan Bradley <bradleyrp@gmail.com>
2019-07-09 22:37:55 -0400

    updated modulefiles for cc

--- modulefiles/cc/conda.lua
+++ modulefiles/cc/conda.lua
@@ -1,17 +1,39 @@
-help(
-[[Community-collections conda envrionment.
-This module is for reference only. It provides the conda executable installed
-by community collections via miniconda. This does not load the python 
-environment used by community-collections but instead simply simulates the use 
-of "source miniconda/etc/profile.d/conda.sh" which is the preferred method. 
-Note that users will also have to update their RC files in the usual way, to 
-properly use "conda" after loading this module.
-]])
--- via: echo "source  miniconda/etc/profile.d/conda.sh" > tmp.sh
--- via: $LMOD_DIR/sh_to_modulefile ./tmp.sh > tmp.lua
-setenv("CONDA_EXE","/home/user/community-collections/miniconda/bin/conda")
-setenv("CONDA_PYTHON_EXE","/home/user/community-collections/miniconda/bin/python")
-setenv("CONDA_SHLVL","0")
-prepend_path("PATH","/home/user/community-collections/miniconda/condabin")
-setenv("_CE_CONDA","")
-setenv("_CE_M","")
+local root = pathJoin(os.getenv("_COMCOL_ROOT"),'miniconda')
+setenv("ANACONDA3ROOT", root)
+setenv("PYTHONROOT", root)
+local python_version = capture(root .. "/bin/python -V |& awk '{print $2}'")
+local conda_version = capture(root .. "/bin/conda --version |& awk '{print $2}'")
+function trim(s)
+   return (s:gsub("^%s*(.-)%s*$", "%1"))
+end
+conda_version = trim(conda_version)
+help([[ Loads the Miniconda environment supporting Community-Collections. ]])
+whatis("Sets the environment to use the Community-Collections Miniconda.")
+local conda_exe = os.getenv("CONDA_EXE")
+local myShell = myShellName()
+-- rpb removes two prepends of bin and condabin to prevent lingering conda
+-- if CONDA_EXE is unset then trigger source of conda.{sh,csh}
+if (conda_exe == nil or conda_exe == "") then
+  if (myShell == "bash") then
+      cmd = "source " .. root .. "/etc/profile.d/conda.sh"
+  else
+      cmd = "source " .. root .. "/etc/profile.d/conda.csh"
+  end
+  execute{cmd=cmd, modeA = {"load"}}
+else
+  if (myShell == "bash") then
+      cmd = "conda deactivate; " ..
+            "unset ANACONDA3ROOT; unset PYTHONROOT; unset CONDA_EXE; " ..
+            "unset CONDA_PYTHON_EXE; unset _CE_CONDA; unset _CE_M; " ..
+            "unset __add_sys_prefix_to_path; unset __conda_activate; " ..
+            "unset __conda_reactivate; unset __conda_hashr; unset CONDA_SHLVL; " ..
+            "unset conda"
+  else
+      cmd = "conda deactivate; " ..
+            "unsetenv CONDA_EXE; unsetenv _CONDA_ROOT; unsetenv _CONDA_EXE; " ..
+            "unsetenv CONDA_PYTHON_EXE; unset CONDA_SHLVL; unalias conda"
+  end
+  execute{cmd=cmd, modeA = {"unload"}}
+  -- rpb removes condabin so conda cannot be accessed after unloading
+  remove_path("PATH",pathJoin(os.getenv("_COMCOL_ROOT"),"miniconda","condabin"))
+end
--- modulefiles/cc/env.lua
+++ modulefiles/cc/env.lua
@@ -1,23 +1,11 @@
-help(
-[[Community-collections python envrionment.
-This module is for reference only. It provides the conda executable installed
-by community collections via miniconda. This does not load the python 
-environment used by community-collections but instead simply simulates the use 
-of "source miniconda/etc/profile.d/conda.sh" which is the preferred method. 
-Note that users will also have to update their RC files in the usual way, to 
-properly use "conda" after loading this module.
-]])
--- !? development note: what is the canonical way to 
--- via: echo "source  miniconda/etc/profile.d/conda.sh && conda activate community-collections" > tmp.sh
--- via: $LMOD_DIR/sh_to_modulefile ./tmp.sh > tmp.lua
-setenv("CONDA_DEFAULT_ENV","community-collections")
-setenv("CONDA_EXE","/home/user/community-collections/miniconda/bin/conda")
-setenv("CONDA_PREFIX","/home/user/community-collections/miniconda/envs/community-collections")
-setenv("CONDA_PROMPT_MODIFIER","(community-collections) ")
-setenv("CONDA_PYTHON_EXE","/home/user/community-collections/miniconda/bin/python")
-setenv("CONDA_SHLVL","1")
-prepend_path("PATH","/home/user/community-collections/miniconda/condabin")
-prepend_path("PATH","/home/user/community-collections/miniconda/envs/community-collections/bin"
-)
-setenv("_CE_CONDA","")
-setenv("_CE_M","")
+local conda_prefix = os.getenv("CONDA_PREFIX")
+if (conda_prefix == nil or conda_prefix == "") then
+  -- !!! is activation the same in csh?
+  load('cc/conda')
+  cmd = "conda activate community-collections"
+  execute{cmd=cmd, modeA = {"load"}}
+else
+  cmd = "conda deactivate"
+  execute{cmd=cmd, modeA = {"unload"}}
+  unload('cc/conda')
+end
