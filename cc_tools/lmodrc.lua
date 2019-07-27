1a181c83c2d592ddd26c5b3f663b6f693af2e85c
Ryan Bradley <bradleyrp@gmail.com>
2019-07-27 08:41:41 -0400

    building property tags

---
+++ cc_tools/lmodrc.lua
@@ -0,0 +1,10 @@
+local i18n = require("i18n")
+propT = {
+   cc_status = {
+      validT = { downloaded = 1, available = 1 },
+      displayT = {
+         ready = { short = "(r)",  color = "green", doc = "Downloaded  and ready to use (community collections)."},
+         available = { short = "(a)",  color = "black", doc = "Available for download (community collections)."},
+     },
+   },
+}
