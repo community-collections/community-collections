6134e07ba5b12fa7e7c93f452d5a9b6f332c5979
Kevin Manalo <kmanalo@gmail.com>
2019-08-05 23:57:36 -0400

    more coverage testing

--- test/interface_test.py
+++ test/interface_test.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python
 
 from interface import Interface
-import argparse
-import cc_tools
-from cc_tools.statetools import Cacher
-from cc_tools.statetools import Parser
 import sys
+import os
 
-# try:
-#     from unittest import mock  # python 3.3+
-# except ImportError:
-#     import mock  # python 2.6-3.2
 try:
-        # python 3.4+ should use builtin unittest.mock not mock package
-            from unittest.mock import patch
+    # python 3.4+ should use builtin unittest.mock not mock package
+    from unittest.mock import patch
 except ImportError:
-        from mock import patch
+    from mock import patch
 
-#@mock.patch('argparse.ArgumentParser.parse_args',
-#            return_value=argparse.Namespace())
+
 def test_flake8():
     """
     Test the flake8 command where we compare the files under flake8 inspection
@@ -27,7 +19,27 @@ def test_flake8():
     with patch.object(sys, 'argv', ['cc', 'flake8']):
         pyfiles = Interface().flake8()
     assert ['cc_tools/__init__.py', 'cc_tools/execute.py',
-           'cc_tools/installers.py', 'cc_tools/misc.py',
-           'cc_tools/modulefile_templates.py', 'cc_tools/settings.py',
-           'cc_tools/statetools.py', 'cc_tools/stdtools.py',
-           'interface.py'] == pyfiles
+            'cc_tools/installers.py', 'cc_tools/misc.py',
+            'cc_tools/modulefile_templates.py', 'cc_tools/settings.py',
+            'cc_tools/statetools.py', 'cc_tools/stdtools.py',
+            'interface.py'] == pyfiles
+
+
+def test_profile_cc_file():
+    """
+    Test the noninteractive command './cc profile --no-bashrc'
+    """
+    # the cli arg matches the method call
+    with patch.object(sys, 'argv', ['cc', 'profile', '--no-bashrc']):
+        Interface().profile(bashrc=False)
+    assert os.path.exists('profile_cc.sh')
+
+
+def test_capable():
+    """
+    Test the noninteractive command './cc capable'
+    There is not an assertion to test yet because this is system dependent
+    """
+    # the cli arg matches the method call
+    with patch.object(sys, 'argv', ['cc', 'capable']):
+        Interface().capable()
